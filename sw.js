26e8a8f18bcac3693c37399fc7b90f9e5f4ccc63
GuardHonour <shahryarmalik551@gmail.com>
2026-09-22 16:55:05 +1000

    Add files via upload
    forge update 2026-08-25i

--- sw.js
+++ sw.js
@@ -12,9 +12,10 @@
  * BUILD must be bumped in step with APP_BUILD in index.html. Bumping it deletes
  * every older cache on activate, which is what makes an update actually land.
  */
-const BUILD = '2026-08-25h';
+const BUILD = '2026-08-25i';
 const CACHE = 'forge-' + BUILD;
-const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon-96.png'];
+const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
+  './icon-maskable-512.png', './favicon-96.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil((async () => {
